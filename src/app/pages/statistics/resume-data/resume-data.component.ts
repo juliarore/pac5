b554d603f378d7efccdfa295c5ed7b478b9784e5
Júlia Romero <juliarore6@gmail.com>
2026-06-07 19:54:59 +0200

    feat: create application structure with standalone pages

---
+++ src/app/pages/statistics/resume-data/resume-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume-data.component.html',
+  styleUrl: './resume-data.component.scss'
+})
+export class ResumeDataComponent {
+
+}
