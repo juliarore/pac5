93498354db48959aed179035ada3d98059a0205c
Júlia Romero <juliarore6@gmail.com>
2026-06-14 21:07:08 +0200

    feat: implement deferred loading with viewport triggers and loading placeholders

--- src/app/pages/statistics/statistics-page.component.ts
+++ src/app/pages/statistics/statistics-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { GenderComponent } from './gender/gender.component';
 import { GeneralResultsComponent } from './general-results/general-results.component';
 import { ResumeDataComponent } from './resume-data/resume-data.component';
@@ -6,7 +7,12 @@ import { ResumeDataComponent } from './resume-data/resume-data.component';
 @Component({
   selector: 'app-statistics-page',
   standalone: true,
-  imports: [ResumeDataComponent, GenderComponent, GeneralResultsComponent],
+  imports: [
+    ResumeDataComponent,
+    GenderComponent,
+    GeneralResultsComponent,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './statistics-page.component.html',
   styleUrl: './statistics-page.component.scss',
 })
