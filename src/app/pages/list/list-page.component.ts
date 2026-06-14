aff68a7431d05e55dde3454361ad742c82ab5381
Júlia Romero <juliarore6@gmail.com>
2026-06-14 15:04:31 +0200

    feat: implement student list page with virtual scrolling

--- src/app/pages/list/list-page.component.ts
+++ src/app/pages/list/list-page.component.ts
@@ -1,12 +1,31 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { Component } from '@angular/core';
+import { StudentDTO } from '../../models/student.dto';
+import { StudentService } from '../../services/student.service';
 
 @Component({
   selector: 'app-list-page',
   standalone: true,
-  imports: [],
+  imports: [ScrollingModule],
   templateUrl: './list-page.component.html',
-  styleUrl: './list-page.component.scss'
+  styleUrl: './list-page.component.scss',
 })
 export class ListPageComponent {
+  students: StudentDTO[] = [];
 
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit() {
+    this.loadStudents();
+  }
+
+  loadStudents(): void {
+    this.studentService.getStudents().subscribe((students) => {
+      this.students = students;
+    });
+  }
+
+  trackByStudentId(index: number, student: StudentDTO): number {
+    return student.id;
+  }
 }
